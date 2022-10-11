83cd363f8b054b37cd6cd02d4d63e2900bbab325
Clink <63194775+Clink-droid@users.noreply.github.com>
2022-10-11 16:58:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Hi this is student profile page of ByExpertise
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
